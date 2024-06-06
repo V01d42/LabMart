5025a664d51d3198c41f800a1676a78150de6ab5
V01d42 <89440437+V01d42@users.noreply.github.com>
2024-06-06 13:16:42 +0900

    add get data from database

--- FastAPI/crud/user.py
+++ FastAPI/crud/user.py
@@ -5,20 +5,20 @@ from db.models import user as models
 from core.security import get_password_hash
 
 
-# def get_user(db: Session, user_id: str):
-#     return db.query(models.User).filter(models.User.id == user_id).first()
+def get_user(db: Session, user_id: str):
+    return db.query(models.User).filter(models.User.id == user_id).first()
 
 
-# def get_user_by_email(db: Session, email: str):
-#     return db.query(models.User).filter(models.User.email == email).first()
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
 
 
-# def get_user_by_username(db: Session, username: str):
-#     return db.query(models.User).filter(models.User.username == username).first()
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
 
 
-# def get_users(db: Session, skip: get_user = 0, limit: get_user = 100):
-#     return db.query(models.User).offset(skip).limit(limit).all()
+def get_users(db: Session, skip: get_user = 0, limit: get_user = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
 
 
 def create(db: Session, user: UserCreate):
