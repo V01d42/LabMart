d6259a149dd52122b9c9a48ba0b506e6ec2ff3e6
V01d42 <89440437+V01d42@users.noreply.github.com>
2024-06-06 13:00:19 +0900

    add user schemas setting

---
+++ FastAPI/schemas/user.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+
+class UserBase(BaseModel):
+    email: str
+    username: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    hashed_password: str
+
+    class Config:
+        orm_mode = True
