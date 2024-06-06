855af39b41a36dc6b5f1ccbd09456e647aacbe39
V01d42 <89440437+V01d42@users.noreply.github.com>
2024-06-06 14:14:01 +0900

    rename core to services

---
+++ FastAPI/services/auth.py
@@ -0,0 +1,29 @@
+from datetime import timedelta, datetime
+
+from jose import jwt, JWTError
+from sqlalchemy.orm import Session
+
+from crud import user as crud_user
+from services.security import verify_password
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def authenticate_user(db: Session, username: str, password: str):
+    user = crud_user.get_user_by_username(db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
