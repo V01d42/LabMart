b47d30a58772710095a1b3389aa2666db93a1d5a
V01d42 <89440437+V01d42@users.noreply.github.com>
2024-06-06 13:21:25 +0900

    fix class name

--- FastAPI/crud/user.py
+++ FastAPI/crud/user.py
@@ -6,23 +6,23 @@ from core.security import get_password_hash
 
 
 def get_user(db: Session, user_id: str):
-    return db.query(models.User).filter(models.User.id == user_id).first()
+    return db.query(models.DbUser).filter(models.DbUser.id == user_id).first()
 
 
 def get_user_by_email(db: Session, email: str):
-    return db.query(models.User).filter(models.User.email == email).first()
+    return db.query(models.DbUser).filter(models.DbUser.email == email).first()
 
 
 def get_user_by_username(db: Session, username: str):
-    return db.query(models.User).filter(models.User.username == username).first()
+    return db.query(models.DbUser).filter(models.DbUser.username == username).first()
 
 
 def get_users(db: Session, skip: get_user = 0, limit: get_user = 100):
-    return db.query(models.User).offset(skip).limit(limit).all()
+    return db.query(models.DbUser).offset(skip).limit(limit).all()
 
 
 def create(db: Session, user: UserCreate):
-    new_user = models.User(
+    new_user = models.DbUser(
         email=user.email,
         username=user.username,
         hashed_password=get_password_hash(user.password)
