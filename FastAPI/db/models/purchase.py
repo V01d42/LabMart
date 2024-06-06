74c520af21b7fd2262cc16bb86879164707e24fb
V01d42 <89440437+V01d42@users.noreply.github.com>
2024-06-07 07:24:31 +0900

    fix relationship setting

--- FastAPI/db/models/purchase.py
+++ FastAPI/db/models/purchase.py
@@ -1,5 +1,6 @@
 from db.session import Base
 from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.schema import ForeignKey
 from sqlalchemy.orm import relationship
 
 
@@ -8,7 +9,7 @@ class DbPurchase(Base):
     __tablename__ = "purchase"
     id = Column(Integer, primary_key=True, index=True)
     product_id = Column(Integer)
-    user_id = Column(Integer)
+    user_id = Column(Integer, ForeignKey("users.id", nullable=False))
     quantity = Column(Integer)
     total_price = Column(String)
     purchase_date = Column(DateTime)
