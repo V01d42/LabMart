011e7d767722be695a761f7af1d419455050ba5b
V01d42 <89440437+V01d42@users.noreply.github.com>
2024-06-07 08:13:52 +0900

    add product database models

---
+++ FastAPI/db/models/product.py
@@ -0,0 +1,17 @@
+from db.session import Base
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.schema import ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class DbProduct(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    description = Column(String)
+    store_id = Column(Integer)
+    price = Column(Integer)
+    stock = Column(Integer)
+    admin_id = Column(Integer)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
