889b3297c97bd50130a8cb178399187fa784f9d6
V01d42 <89440437+V01d42@users.noreply.github.com>
2024-06-07 19:38:54 +0900

    change to get SECRET_KEY from .env

--- FastAPI/services/security.py
+++ FastAPI/services/security.py
@@ -3,13 +3,18 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import jwt, JWTError
 from sqlalchemy.orm import Session
 from starlette import status
+from starlette.config import Config
+from starlette.datastructures import Secret
 
 from api.deps import get_db
 from crud import user as crud_user
 from db.models.user import DbUser
 from schemas.token import TokenData, Token
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+
+config = Config("../.env")
+
+SECRET_KEY = config("SECRET_KEY", cast=Secret)
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
