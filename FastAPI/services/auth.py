4aabb95f415b3dccfd06b47ca12a86201dd1d0ce
V01d42 <89440437+V01d42@users.noreply.github.com>
2024-06-07 19:09:52 +0900

    fix depricated parts

--- FastAPI/services/auth.py
+++ FastAPI/services/auth.py
@@ -1,4 +1,4 @@
-from datetime import timedelta, datetime
+from datetime import timedelta, timezone, datetime
 
 from jose import jwt, JWTError
 from sqlalchemy.orm import Session
@@ -21,9 +21,9 @@ def authenticate_user(db: Session, username: str, password: str):
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
