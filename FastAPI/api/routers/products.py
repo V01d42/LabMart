88901c12a03af61a675f9d2f899af7a1d47b5055
V01d42 <89440437+V01d42@users.noreply.github.com>
2024-06-08 17:48:02 +0900

    Revert "fix product router"
    This reverts commit 7cfca960daac407ef138144ab8a4e0683e095764.

--- FastAPI/api/routers/products.py
+++ FastAPI/api/routers/products.py
@@ -14,11 +14,11 @@ router = APIRouter(
 )
 
 
-@router.get("", response_model=List[Product])
-def read_product(db: Session = Depends(get_db)):
-    return crut_product.get_products(db=db)
-
-
 @router.post("", response_model=Product)
 def create_product(request: ProductCreate, db: Session = Depends(get_db)):
     return crut_product.create(db, request)
+
+
+@router.get("", response_model=List[Product])
+def read_product(db: Session = Depends(get_db)):
+    return crut_product.get_products(db=db)
