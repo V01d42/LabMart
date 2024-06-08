e2dc05f11ee05ffa750570da05a1116061e76601
idkiy0178 <sc24758f@st.omu.ac.jp>
2024-06-09 03:26:00 +0900

    一旦コミット

--- React/src/pages/PurchasePage.tsx
+++ React/src/pages/PurchasePage.tsx
@@ -63,86 +63,12 @@
 //     });
 //   };
 
-//   const handlePurchaseProcess = () => {
-//     // 購入後の処理
-//   };
-
-//   return (
-//     <div>
-//       <SelectGroup onGroupSelect={handleGroupSelect} />
-//       <SelectProduct
-//         selectedGroup={selectedGroup}
-//         selectedProducts={selectedProducts}
-//         onProductSelect={handleProductSelect}
-//       />
-//       <Button colorScheme="blue" mt='10' onClick={onOpen}>購入</Button>
-//       <PurchaseConfirm
-//         selectedProducts={selectedProducts}
-//         isOpen={isOpen}
-//         onClose={onClose}
-//         onConfirm={handlePurchaseProcess}
-//       />
-//     </div>
-//   );
-// };
-
-// export default PurchasePage;
-
-
-
-// import React, { useState } from 'react';
-// import { Button, useDisclosure } from '@chakra-ui/react';
-// import SelectGroup from '../components/SelectGroup';
-// import SelectProduct from '../components/SelectProduct';
-// import PurchaseConfirm from '../components/PurchaseConfirm';
-
-// interface Product {
-//   name: string;
-//   description: string;
-//   store_id: number;
-//   price: number;
-//   stock: number;
-//   id: number;
-// }
-
-// interface SelectedProduct extends Product {
-//   quantity: number;
-// }
-
-// const PurchasePage: React.FC = () => {
-//   const [selectedGroup, setSelectedGroup] = useState<number>(319);
-//   const [selectedProducts, setSelectedProducts] = useState<SelectedProduct[]>([]);
-//   const { isOpen, onOpen, onClose } = useDisclosure();
-//   const [isPurchaseComplete, setIsPurchaseComplete] = useState(false);
-
-//   const handleGroupSelect = (group: number) => {
-//     setSelectedGroup(group);
-//   };
-
-//   const handleProductSelect = (product: SelectedProduct, quantity: number) => {
-//     setSelectedProducts((prevProducts) => {
-//       if (quantity === 0) {
-//         return prevProducts.filter((p) => p.id !== product.id);
-//       } else {
-//         const existingProduct = prevProducts.find((p) => p.id === product.id);
-//         if (existingProduct) {
-//           existingProduct.quantity = quantity;
-//           return [...prevProducts];
-//         } else {
-//           return [...prevProducts, { ...product, quantity }];
-//         }
-//       }
-//     });
-//   };
-
-//   const handlePurchaseProcess = () => {
-//     setIsPurchaseComplete(true);
-//     // 購入後の処理
-//     console.log(selectedProducts)
-//   };
-
-//   const handleContinueShopping = () => {
-//     setIsPurchaseComplete(false);
+//   const handleConfirm = () => {
+//     // ここに購入後の処理を記述する (今は購入品をコンソールで表示するだけ)
+//     console.log('Purchased:', selectedProducts);
+//     {selectedProducts.map((product, index) => (
+//       console.log(product.name)
+//     ))}
 //     onClose();
 //   };
 
@@ -158,16 +84,14 @@
 //       <PurchaseConfirm
 //         selectedProducts={selectedProducts}
 //         isOpen={isOpen}
-//         onClose={handleContinueShopping}
-//         onConfirm={handlePurchaseProcess}
-//         isPurchaseComplete={isPurchaseComplete}
+//         onClose={onClose}
+//         onConfirm={handleConfirm}
 //       />
 //     </div>
 //   );
 // };
 
 // export default PurchasePage;
-
 
 
 
@@ -223,10 +147,10 @@ const PurchasePage: React.FC = () => {
       total_price: product.price * product.quantity
     }));
 
-    // console.log(purchaseData)
+    console.log(purchaseData)
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/purchases", {
+      const response = await fetch("http://127.0.0.1:8000/users", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -248,10 +172,6 @@ const PurchasePage: React.FC = () => {
     }
   };
 
-  const handleConfirm = () => {
-    handlePurchaseProcess();
-  };
-
   const handleContinueShopping = () => {
     setIsPurchaseComplete(false);
     onClose();
@@ -270,7 +190,7 @@ const PurchasePage: React.FC = () => {
         selectedProducts={selectedProducts}
         isOpen={isOpen}
         onClose={handleContinueShopping}
-        onConfirm={handleConfirm}
+        onConfirm={handlePurchaseProcess}
         isPurchaseComplete={isPurchaseComplete}
       />
     </div>
