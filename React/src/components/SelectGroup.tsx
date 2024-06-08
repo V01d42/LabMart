8e18f57bb74e42b94ebdc6702aeb1a164ccf4a6a
idkiy0178 <sc24758f@st.omu.ac.jp>
2024-06-09 04:54:46 +0900

    リファクタリング

--- React/src/components/SelectGroup.tsx
+++ React/src/components/SelectGroup.tsx
@@ -1,34 +1,3 @@
-// import React from 'react';
-// import { Tabs, TabList, Tab } from '@chakra-ui/react';
-// import { useStore } from './StoreidStatus';
-
-// const SelectGroup: React.FC = () => {
-//   const { selectedStoreId, setSelectedStoreId } = useStore();
-
-//   const handleTabChange = (index: number) => {
-//     const storeIds = [319, 324, 405];
-//     setSelectedStoreId(storeIds[index]);
-//   };
-
-//   const getTabIndex = () => {
-//     const storeIds = [319, 324, 405];
-//     return storeIds.indexOf(selectedStoreId);
-//   };
-
-//   return (
-//     <Tabs index={getTabIndex()} onChange={handleTabChange}>
-//       <TabList>
-//         <Tab>319</Tab>
-//         <Tab>324</Tab>
-//         <Tab>405</Tab>
-//       </TabList>
-//     </Tabs>
-//   );
-// };
-
-// export default SelectGroup;
-
-
 import { Tab, TabList, Tabs } from '@chakra-ui/react';
 import React from 'react';
 
