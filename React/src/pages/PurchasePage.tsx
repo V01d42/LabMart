bf59f9dbd997be5cd327ad88a7854ea204b65d4a
idkiy0178 <sc24758f@st.omu.ac.jp>
2024-06-09 03:29:23 +0900

    一旦コミット

--- React/src/pages/PurchasePage.tsx
+++ React/src/pages/PurchasePage.tsx
@@ -150,7 +150,7 @@ const PurchasePage: React.FC = () => {
     console.log(purchaseData)
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/users", {
+      const response = await fetch("http://127.0.0.1:8000/purchases", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
