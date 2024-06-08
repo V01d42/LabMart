a9e166f3510ebc513d4d277d21b992b4a9efc80a
TomoyaOgawa <5stream418@gmail.com>
2024-06-08 15:55:06 +0900

    add path to AdminPage

--- React/src/Routes.tsx
+++ React/src/Routes.tsx
@@ -1,13 +1,14 @@
 import { RouteObject } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
 import PurchasePage from "./pages/PurchasePage";
 import SigninPage from "./pages/SigninPage";
+import AdminPage from "./pages/AdminPage";
 
 const Routes: RouteObject[] = [
   {
     children: [
-        { path: "/", element: <SigninPage /> },
-        { path: "/purchase", element: <PurchasePage /> },
+      { path: "/", element: <SigninPage /> },
+      { path: "/purchase", element: <PurchasePage /> },
+      { path: "/admin", element: <AdminPage /> },
     ],
   },
 ];
