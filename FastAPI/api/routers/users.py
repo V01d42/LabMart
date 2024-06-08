a2ea90af017edf9dc16440becea2d22d0ca83a4d
V01d42 <89440437+V01d42@users.noreply.github.com>
2024-06-08 16:15:12 +0900

    fix import name

--- FastAPI/api/routers/users.py
+++ FastAPI/api/routers/users.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm.session import Session
 
 from services.security import get_current_active_user
 from api.deps import get_db
-from crud import user as user_crud
+from crud import user as crud_user
 
 
 router = APIRouter(
@@ -19,7 +19,7 @@ async def read_users_me(current_user: User = Depends(get_current_active_user)):
 
 @router.post("", response_model=User)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    db_user = user_crud.get_user_by_username(db, username=user.username)
+    db_user = crud_user.get_user_by_username(db, username=user.username)
     if db_user:
         raise HTTPException(status_code=400, detail="username already registered")
-    return user_crud.create(db=db, user=user)
+    return crud_user.create(db=db, user=user)
