a7a7b77a82ff54aaec2213d4d76749479606a4aa
idkiy0178 <sc24758f@st.omu.ac.jp>
2024-06-09 13:28:33 +0900

    購入画面のデザインを修正

--- React/src/components/SelectProduct.tsx
+++ React/src/components/SelectProduct.tsx
@@ -1,4 +1,13 @@
-import { Card, CardBody, Select, Text } from "@chakra-ui/react";
+import {
+  Select,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import React from "react";
 
 interface Product {
@@ -25,42 +34,58 @@ const SelectProduct: React.FC<ProductListProps> = ({
   selectedGroup,
   selectedProducts,
   onProductSelect,
-  products
+  products,
 }) => {
   const handleQuantityChange = (product: Product, quantity: number) => {
     onProductSelect({ ...product, quantity }, quantity);
   };
 
   return (
-    <div>
-      {products.map((product) => {
-        const selectedProduct = selectedProducts.find(
-          (p) => p.id === product.id
-        );
-        const initialQuantity = selectedProduct ? selectedProduct.quantity : 0;
+    <>
+      <TableContainer mt="10">
+        <Table size="sm" variant="simple">
+          <Thead>
+            <Tr>
+              <Th>商品名</Th>
+              <Th>金額(円)</Th>
+              <Th>購入数</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {products.map((product) => {
+              const selectedProduct = selectedProducts.find(
+                (p) => p.id === product.id
+              );
+              const initialQuantity = selectedProduct
+                ? selectedProduct.quantity
+                : 0;
 
-        return (
-          <Card key={product.id} mt="5">
-            <CardBody>
-              <Text>{product.name}</Text>
-              <Select
-                size="sm"
-                value={initialQuantity}
-                onChange={(e) =>
-                  handleQuantityChange(product, parseInt(e.target.value, 10))
-                }
-              >
-                {[...Array(product.stock + 1).keys()].map((i) => (
-                  <option key={i} value={i}>
-                    {i}
-                  </option>
-                ))}
-              </Select>
-            </CardBody>
-          </Card>
-        );
-      })}
-    </div>
+              return (
+                <Tr key={product.id}>
+                  <Td>{product.name}</Td>
+                  <Td>{product.price}</Td>
+                  <Td>
+                  <Select
+                    size="sm"
+                    value={initialQuantity}
+                    onChange={(e) =>
+                      handleQuantityChange(product, parseInt(e.target.value, 10))
+                    }
+                  >
+                      {[...Array(product.stock + 1).keys()].map((i) => (
+                        <option key={i} value={i}>
+                          {i}
+                        </option>
+                      ))}
+                    </Select>
+                  </Td>
+                </Tr>
+              );
+            })}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </>
   );
 };
 
